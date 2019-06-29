51ab6549d98482c55e9e29d2c64d588c0c49dd3d
Sunilpore <sunilpore20@gmail.com>
2019-06-29 20:42:47 +0530

    study @Disposable operator.

--- RxJava/app/src/main/java/com/rxjavaeg1/MainActivity.java
+++ RxJava/app/src/main/java/com/rxjavaeg1/MainActivity.java
@@ -8,18 +8,28 @@ import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
+
+/**
+ * @subscribe -> when not contains onSubscribe(), then it will return Disposable object.
+ */
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = "MainActivityTag";
 
+    CompositeDisposable disposable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        disposable = new CompositeDisposable();
 
         Observable<Task> taskObservable = Observable
                 .fromIterable(DataSource.createTasksList())
@@ -27,10 +37,9 @@ public class MainActivity extends AppCompatActivity {
                 .filter(new Predicate<Task>() {
                     @Override
                     public boolean test(Task task) throws Exception {
-                        
+
                         //Used for non blocking UI thread operation
                         //After task.isComplete() onNext() is called for every iteration
-
                         try {
                             Thread.sleep(1000);
                         } catch (InterruptedException e) {
@@ -66,7 +75,28 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        disposable.add(taskObservable.subscribe(new Consumer<Task>() {
+            @Override
+            public void accept(Task task) throws Exception {
+
+            }
+        }));
+
+
     }
 
+
+    /**
+     * @disosable -> normally called on onDestroy() of an activity and onClear of an ViewModel()
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //It will like soft delete.Delete all disposable object, but allow continue to subscribe on Observable
+        disposable.clear();
+
+        //It will like hard delete. Delete all objects, but it will disable the Observable to observe further
+        disposable.dispose();
+    }
 
 }
